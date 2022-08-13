ceef43e5fe91aaff243ba7c8edc24c0f623fee47
varsha <varshaprajapati.15004@gmail.com>
2022-08-13 20:45:24 +0530

    Updates

---
+++ precache-manifest.d0f44c9dc2b4004704f27539148aa396.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78158e1b993f1827b369a2e61f03aa8",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "05a706aa5887da0b41ac",
+    "url": "/Keeper/static/js/2.0bc676c4.chunk.js"
+  },
+  {
+    "revision": "ff9b9b9c3bfff550ba67",
+    "url": "/Keeper/static/js/main.10c67295.chunk.js"
+  },
+  {
+    "revision": "573da01c7c909d69a3f3",
+    "url": "/Keeper/static/js/runtime-main.13d65c7f.js"
+  }
+]);
